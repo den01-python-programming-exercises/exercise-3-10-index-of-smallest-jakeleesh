fc0c41fccea6a526fe48fd283d4d1996c3f6039d
Jake Lee <jake.lee@fdmgroup.com>
2021-09-28 21:20:05 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #write your code below this line
+    nums = []
+    while True:
+        num = int(input())
+        if (num == 9999):
+            break
+        else:
+            nums.append(num)
+    smallest = nums[0]
+    for i in range(len(nums)):
+        if (nums[i] < smallest):
+            smallest = nums[i]
+    print("Smallest number: " + str(smallest))
+    for j in range(len(nums)):
+        if (nums[j] == smallest):
+            print("Found at index: " + str(j))
 
 if __name__ == '__main__':
     main()
